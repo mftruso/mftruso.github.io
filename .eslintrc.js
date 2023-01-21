960179dc81f35479c317bf70d495efe8f544522a
Mike Truso <mftruso@gmail.com>
2023-01-20 21:57:35 -0600

    vitest

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: [
+    'vitest'
   ],
   // add your custom rules here
   rules: {
---
+++ vitest.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+  resolve: {
+    alias: [
+        { find: '@', replacement: path.resolve(__dirname, './') },
+    ]
+  }
+})
