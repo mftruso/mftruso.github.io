377b3018ba3b1580b71373511f557a087b676541
mftruso <mftruso@users.noreply.github.com>
2023-01-21 04:03:39 +0000

    deploy: fac8d3319c6c701a7283c7ebc8dc62e07605b232

---
+++ _nuxt/ProseTbody.b7e130f1.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper.a1a6add7.js";import{o as r,F as t,K as s}from"./entry.71bf2649.js";const n={};function c(e,a){return r(),t("tbody",null,[s(e.$slots,"default")])}const _=o(n,[["render",c]]);export{_ as default};
---
+++ _nuxt/ProseThead.13359134.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper.a1a6add7.js";import{o as r,F as t,K as s}from"./entry.71bf2649.js";const a={};function n(e,c){return r(),t("thead",null,[s(e.$slots,"default")])}const _=o(a,[["render",n]]);export{_ as default};
